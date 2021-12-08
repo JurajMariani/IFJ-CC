30fc279abc101973a1fa76f96879f59ba29f5319
xhoril01 <xhoril01@stud.fit.vutbr.cz>
2021-12-08 20:38:10 +0100

    Analysis' library update vol.5

--- src/libs/analysis.h
+++ src/libs/analysis.h
@@ -3,19 +3,19 @@
 #include <string.h>
 #include "Defs.h"
 
-#include "TS.h"
-
 #define STR_SIZE 10
+#define WHITESPACES ((input_c == 32 || input_c == '\t' || input_c == '\r' || input_c == '\n' || input_c == '\v' || input_c == '\f'))
 
 #define ARR_LEN 17
 char arr_keywords[ARR_LEN][10];
 
+char *output;
+unsigned output_length;
 int lex_err_flag;
 int line_cnt;
 token result;
 int str2write;
 
-void KeyArrInit();
 void first_perimeter( char * , char ** , unsigned * );
 void second_perimeter( char * , char ** , unsigned * );
 void other_states( char * , char ** , unsigned * );
@@ -33,3 +33,5 @@ void two(char *, unsigned *, char *);
 void zero_zero(char *, unsigned *, char *);
 void two_five(char *, unsigned *, char *);
 void others(char *, unsigned *, char *);
+
+void KeyArrInit();
